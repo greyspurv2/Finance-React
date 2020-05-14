e0d0ad45e2d095e17992fcad94c6a9d5042b5494
greyspurv2 <35030409+greyspurv2@users.noreply.github.com>
2020-05-14 21:31:49 +0200

    Add files via upload
    Creating paths to pages and files

---
+++ node.js
@@ -0,0 +1,32 @@
+const path = require("path")
+const express = require ("express")
+
+//function to launch our node app
+
+const app = express()
+
+// use a file from a directory instead of locally from our machines
+
+const PublicDirectoryPath = path.join(__dirname, "../public")
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+
+
+// create routes from our pages
+
+app.get("/crypto", req, res) => {
+    res.send("")
+}
+
+app.get("/stocks", req, res) => {
+    res.send("")
+}
+
+app.get("/news", req, res) => {
+    res.send("")
+}
+
+app.get("/contact", req, res) => {
+    res.send("")
+}
